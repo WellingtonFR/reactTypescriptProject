af92bd006d26d688d576c721af9212e6e0a4f031
WellingtonFR <wellington.rocha@luizalabs.com>
2024-06-25 18:02:24 -0300

    Melhoria na parte visual

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,25 @@ import cardStyle from "./card.module.css";
 export default function Page() {
   return (
     <div className={cardStyle.card}>
-      <h1 className={cardStyle.header}>Pergunta</h1>
+
+      <div className={cardStyle.header}>Pergunta 1 de 10</div>
+
       <p className={cardStyle.question}>A lua gira em torno do sol ?</p>
+
       <form className={cardStyle.form}>
+
+        <div className={cardStyle.answer}>
         <input type="radio" value="sim" id="sim" name="question" />
         <label htmlFor="sim">Sim</label>
+        </div>
+
+        <div className={cardStyle.answer}>
         <input type="radio" value="nao" id="nao" name="question" />
         <label htmlFor="nao">Não</label>
+        </div>
+
       </form>
+      
     </div>
   );
 }
